8523b63fb1ba835a50808c3d4f3c668c9fa71f08
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-04-05 17:33:27 +0530

    JS Practice Questions Done

---
+++ Nirbhay/javascript/practice30426.js
@@ -0,0 +1,15 @@
+//Task1
+console.log("Nirbhay Kumar")
+//Task2
+console.log("2007");
+//Task3
+console.log(2007);
+//Task4
+console.log("Nirbhay Kumar","(2007)")
+//Task5
+console.log("Nirbhay Kumar")
+console.log("(2007)")
+//Task6
+console.log("Nirbhay"+"Kumar"+"(2007)");
+//Task7
+console.log("Nirbhay"+' '+"Kumar"+' '+"(2007)");
