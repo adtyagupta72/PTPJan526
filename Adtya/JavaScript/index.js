5a81c7ad8d9f4a7e79e51b76385ad24ebcd62093
Adtya Gupta <adtyagupta72@gmail.com>
2026-04-06 21:53:57 +0530

    JS: Introduction to Variables WIP

--- Adtya/JavaScript/index.js
+++ Adtya/JavaScript/index.js
@@ -1,1 +1,59 @@
+console.log("Program Starts here------")
 console.log("Hello There....")
+
+//int a = 10
+// var a = 10
+// var b = 20
+// var c = a + b
+// console.log(c)
+
+// a = "Adtya"
+// console.log(a)
+
+// let num = 40
+// let num2 = 60
+// let result = num + num2
+// console.log(result)
+
+// const PI = 3.14
+// const PI_VALUE = 3.14
+
+// let number1 = 10
+// let numberSumResult = number1 + PI_VALUE
+// console.log(numberSumResult)
+
+// //PI = 20
+// console.log(PI)
+
+// console.log(a)//undefined
+// console.log(b)
+
+// var a = 10
+// console.log(a)//10
+// console.log(b)
+
+// console.log(a)
+
+// let a = 10
+// console.log(a)
+
+var globalScopeOut = "I am a Global Scoped Var - OUT"
+let localScopeOut = "I am a Local Scoped Let - OUT"
+//GLOBAL SCOPE
+{
+    //LOCAL SCOPE
+    console.log(globalScopeOut)
+    console.log(localScopeOut)
+    var globalScope = "I am a Global Scoped Var"
+    let localScope = "I am a Local Scoped Let"
+    console.log(globalScope)
+    console.log(localScope)
+    //LOCAL SCOPE
+}
+//GLOBAL SCOPE
+// console.log(globalScope)
+// console.log(localScope)
+console.log(globalScopeOut)
+console.log(localScopeOut)
+
+console.log("Program Ends here------")
