fd06aa9e897f01f95e987c996d164d46c24402ed
Adtya Gupta <adtyagupta72@gmail.com>
2026-04-09 21:18:56 +0530

    JS: Variables

--- Adtya/JavaScript/index.js
+++ Adtya/JavaScript/index.js
@@ -1,69 +1,74 @@
-console.log("Program Starts here------")
-console.log("Hello There....")
+"use strict"
+// console.log("Program Starts here------")
+// console.log("Hello There....")
 
-//int a = 10
-// var a = 10
-// var b = 20
-// var c = a + b
-// console.log(c)
+// //int a = 10
+// // var a = 10
+// // var b = 20
+// // var c = a + b
+// // console.log(c)
 
-// a = "Adtya"
-// console.log(a)
+// // a = "Adtya"
+// // console.log(a)
 
-// let num = 40
-// let num2 = 60
-// let result = num + num2
-// console.log(result)
+// // let num = 40
+// // let num2 = 60
+// // let result = num + num2
+// // console.log(result)
 
-// const PI = 3.14
-// const PI_VALUE = 3.14
+// // const PI = 3.14
+// // const PI_VALUE = 3.14
 
-// let number1 = 10
-// let numberSumResult = number1 + PI_VALUE
-// console.log(numberSumResult)
+// // let number1 = 10
+// // let numberSumResult = number1 + PI_VALUE
+// // console.log(numberSumResult)
 
-// //PI = 20
-// console.log(PI)
+// // //PI = 20
+// // console.log(PI)
 
-// console.log(a)//undefined
-// console.log(b)
+// // console.log(a)//undefined
+// // console.log(b)
 
-// var a = 10
-// console.log(a)//10
-// console.log(b)
+// // var a = 10
+// // console.log(a)//10
+// // console.log(b)
 
-// console.log(a)
+// // console.log(a)
 
-// let a = 10
-// console.log(a)
+// // let a = 10
+// // console.log(a)
 
-var globalScopeOut = "I am a Global Scoped Var - OUT"
-let localScopeOut = "I am a Local Scoped Let - OUT"
-//GLOBAL SCOPE
-{
-    //LOCAL SCOPE
-    console.log(globalScopeOut)
-    console.log(localScopeOut)
-    var globalScope = "I am a Global Scoped Var"
-    let localScope = "I am a Local Scoped Let"
-    console.log(globalScope)
-    console.log(localScope)
-    //LOCAL SCOPE
-}
-//GLOBAL SCOPE
-// console.log(globalScope)
-// console.log(localScope)
-console.log(globalScopeOut)
-console.log(localScopeOut)
+// var globalScopeOut = "I am a Global Scoped Var - OUT"
+// let localScopeOut = "I am a Local Scoped Let - OUT"
+// //GLOBAL SCOPE
+// {
+//     //LOCAL SCOPE
+//     console.log(globalScopeOut)
+//     console.log(localScopeOut)
+//     var globalScope = "I am a Global Scoped Var"
+//     let localScope = "I am a Local Scoped Let"
+//     console.log(globalScope)
+//     console.log(localScope)
+//     //LOCAL SCOPE
+// }
+// //GLOBAL SCOPE
+// // console.log(globalScope)
+// // console.log(localScope)
+// console.log(globalScopeOut)
+// console.log(localScopeOut)
 
-var height
-console.log(height)
-var height
-console.log(height)
+// var height
+// console.log(height)
+// var height
+// console.log(height)
 
-let weight
-console.log(weight)
 // let weight
 // console.log(weight)
+// // let weight
+// // console.log(weight)
 
-console.log("Program Ends here------")
+// console.log("Program Ends here------")
+
+//height = 180
+let height = 180
+console.log(height)
