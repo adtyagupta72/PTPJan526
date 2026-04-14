cb2ae7a47c526f8403f4d23003efd81bd21c9e28
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-04-14 12:54:48 +0530

    Js module2 Assignment Done

---
+++ Nirbhay/javascript/practice130426.js
@@ -0,0 +1,56 @@
+// let PRICE_SINGLE_ROSE=8
+// var NoOfRoses=70
+// let PRICE_SINGLE_LILY=10
+// var NoOfLily=50
+// let PRICE_SINGLE_TULIP=2
+// var NoOfTulip=120
+
+// var TotalPriceRoses=PRICE_SINGLE_ROSE*NoOfRoses
+// var TotalPriceLily=PRICE_SINGLE_LILY*NoOfLily
+// var TotalPriceTulip=PRICE_SINGLE_TULIP*NoOfTulip
+
+// var Total=TotalPriceRoses+TotalPriceLily+TotalPriceTulip
+
+// console.log("Rose",PRICE_SINGLE_ROSE,NoOfRoses,TotalPriceRoses)
+// console.log("Lily",PRICE_SINGLE_LILY,NoOfLily,TotalPriceLily)
+// console.log("Tulip",PRICE_SINGLE_TULIP,NoOfTulip,TotalPriceTulip)
+// console.log("Total Price",Total)
+ 
+// const PRICE_SINGLE_ROSE=8
+// var NoOfRoses=70-20
+
+// const PRICE_SINGLE_LILY=10
+// var NoOfLily=50-30
+
+// const PRICE_SINGLE_TULIP=2
+// var NoOfTulip=120
+
+// var TotalPriceRoses=PRICE_SINGLE_ROSE*NoOfRoses
+// var TotalPriceLily=PRICE_SINGLE_LILY*NoOfLily
+// var TotalPriceTulip=PRICE_SINGLE_TULIP*NoOfTulip
+
+// var Total=TotalPriceRoses+TotalPriceLily+TotalPriceTulip
+
+// console.log("Rose",PRICE_SINGLE_ROSE,NoOfRoses,TotalPriceRoses)
+// console.log("Lily",PRICE_SINGLE_LILY,NoOfLily,TotalPriceLily)
+// console.log("Tulip",PRICE_SINGLE_TULIP,NoOfTulip,TotalPriceTulip)
+// console.log("Total Price",Total)
+ 
+//scenerio2
+
+let FirstName="Maxwell Wright"
+let SecondName="Raja Villarreal"
+let ThirdName="Helen Richards"
+
+let Phone1="(0191)7196495"
+let Phone2="08663982895"
+let Phone3="08001111"
+
+let email1="maxwell@gmail.com"
+let email2="rajacholavill@gmail.com"
+let email3="helenrichard@gmail.com"
+
+console.log("Name"," ","PhoneNumber"," ","Email")
+console.log(FirstName,"",Phone1,"",email1)
+console.log(SecondName,"",Phone2,"",email2)
+console.log(ThirdName,"",Phone3,"",email3)
