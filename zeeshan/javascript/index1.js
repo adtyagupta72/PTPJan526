3fad3c1ca2852920cdc4d332071eba9c0287d88c
zeeshan <zeeshan6767siddiqui@gmail.com>
2026-04-14 19:42:20 +0530

    Module 2 assignment

---
+++ zeeshan/javascript/index1.js
@@ -0,0 +1,68 @@
+
+//  console.log("Zeeshan Siddiqui") //Name Output
+// console.log("02.04.2005") //Date Of Birth
+// // console.log(02 04 2005)
+// console.log("abc","def","ghi") //printing random string
+// console.log("Zeeshan Siddiqui(2005)") // name with year
+// console.log()
+//    let greet="hello"
+//    let count=100
+//    console.log(greet)
+//     greet=1
+//    console.log(greet)
+//    greet="hii"
+//    greet= greet + count
+// console.log(greet)
+
+
+// function addd(){
+//     a=4
+//     b=5
+//     c=a+b
+//     console.log(c)
+// }
+// console.log("answer")nn
+// addd()
+// let count=100
+// console.log(count)
+// {
+//     count = 200
+//     console.log(count)
+// }
+// console.log(count)
+
+
+// let count=100
+// console.log(count)
+// {
+//     let count = 200
+//     console.log(count)
+// }
+// console.log(count)
+// var height = 100
+// console.log(height)
+// console.log(weight)
+// var weight=200
+// console.log(weight)
+
+// let h= 100
+// console.log(h)
+// console.log(w)
+// let w = 200
+// console.log(w)
+
+myPlace = "bengal"
+console.log(myPlace)
+var myPlace
+let myname = "zeeshan"
+sayHi()
+function sayHi()
+{
+    let greet = "hii"
+    console.log(greet,myname)
+}
+sayHello()
+var sayHello = function()
+{
+    console.log(myname)
+}
---
+++ zeeshan/javascript/index3.js
@@ -0,0 +1,19 @@
+let singleRose = 8
+let singleTulip = 2
+let singleLily  = 10
+
+let quantityRose = 70
+let quantityTulip = 120
+let quantityLily = 50
+
+ let valueRose = singleRose * quantityRose
+ let valueTulip = singleTulip * quantityTulip
+ let valueLily = singleLily * quantityLily
+
+ let total = valueRose + valueTulip + valueLily
+
+
+console.log("Rose-unit price:"+ singleRose +", quantity:"+ quantityRose + ", Value:"+ valueRose)
+console.log("Lily-unit price:"+ singleLily +", quantity:"+ quantityTulip + ", Value:"+ valueLily)
+console.log("Tulip-unit price:"+ singleTulip +", quantity:"+ quantityTulip + ", Value:"+ valueTulip)
+console.log("Total:"+total)
