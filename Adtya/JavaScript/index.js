b0d97b9250fb143ff45d7c2deb6f3378ee10c9a6
Adtya Gupta <adtyagupta72@gmail.com>
2026-04-21 07:43:45 +0530

    JS: Implicit Type conversion WIP

--- Adtya/JavaScript/index.js
+++ Adtya/JavaScript/index.js
@@ -437,12 +437,19 @@ Falsy -> "", 0, false, [], {}, undefined, NaN, null
 // console.log(Boolean(undefined)) // -> false
 // console.log(Boolean(null)) // -> false
 
-console.log(BigInt(11)) // -> 11n
-console.log(BigInt(0x11)) // -> 17n
-console.log(BigInt(11e2)) // -> 1100n
-console.log(BigInt(true)) // -> 1n
-console.log(BigInt("11")) // -> 11n
-console.log(BigInt("0x11")) // -> 17n
-console.log(BigInt(null)) // -> Uncaught TypeError: Cannot convert null to a BigInt
-console.log(BigInt(undefined)) // -> Uncaught TypeError: Cannot convert undefined to a BigInt
-console.log(BigInt(NaN)) // -> Uncaught RangeError: The number NaN cannot be converted to a BigInt because it is not an integer
+// console.log(BigInt(11)) // -> 11n
+// console.log(BigInt(0x11)) // -> 17n
+// console.log(BigInt(11e2)) // -> 1100n
+// console.log(BigInt(true)) // -> 1n
+// console.log(BigInt("11")) // -> 11n
+// console.log(BigInt("0x11")) // -> 17n
+// console.log(BigInt(null)) // -> Uncaught TypeError: Cannot convert null to a BigInt
+// console.log(BigInt(undefined)) // -> Uncaught TypeError: Cannot convert undefined to a BigInt
+// console.log(BigInt(NaN)) // -> Uncaught RangeError: The number NaN cannot be converted to a BigInt because it is not an integer
+
+const str1 = 42 + "1"
+console.log(str1)        // -> 421
+console.log(typeof str1) // -> string
+const str2 = 42 - "1"
+console.log(str2)        // -> 41
+console.log(typeof str2) // -> number
