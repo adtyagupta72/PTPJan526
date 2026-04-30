4eb68ad134e2a3324c128ef6d3623303fbe332fe
zeeshan <zeeshan6767siddiqui@gmail.com>
2026-04-30 23:51:27 +0530

    SHift

--- zeeshan/javascript/index3.js
+++ zeeshan/javascript/index3.js
@@ -120,4 +120,11 @@ books.push(a)
 // console.log("Book3;",books[2].title)
 // console.log("Book4;",books[3].title)
 
-console.log(books.slice(2,4))
+// console.log(books.slice(2,4))
+console.log("shifted;",books.shift())
+console.log("length:",books.length)
+
+
+console.log("Book1:",books[0].title)
+console.log("Book2;",books[1].title)                    //title of all books
+console.log("Book3;",books[2].title)
