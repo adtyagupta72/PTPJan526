07d495b5379f76a2a94ac0496efc5033b7d57899
zeeshan <zeeshan6767siddiqui@gmail.com>
2026-04-30 10:38:04 +0530

    Assignent

--- zeeshan/javascript/index3.js
+++ zeeshan/javascript/index3.js
@@ -1,17 +1,89 @@
 // 
-let name1 = "John"
-let name2 = "Zeeshan"
-let name3 = "Samantha"
+// let name1 = "John"
+// let name2 = "Zeeshan"
+// let name3 = "Samantha"
 
-let no1 = 3452617278
-let no2 = 3452617278
-let no3 = 3452617278
+// let no1 = 3452617278
+// let no2 = 3452617278
+// let no3 = 3452617278
 
-let email1 = "johngmail.com"
-let email2 = "zeeshangmail.com"
-let email3 = "samanthagmail.com"
+// let email1 = "johngmail.com"
+// let email2 = "zeeshangmail.com"
+// let email3 = "samanthagmail.com"
 
-console.log("NAME          PH.NUMBER       EMAIL")
-console.log(name1,            no1,           email1)
-console.log(name2,            no2,           email2)
-console.log(name3,            no3,           email3)
+// console.log(name1+"/"+no1+"/"+email1)
+
+// console.log(name2+"/"+no2+"/"+email2)
+// console.time()
+// let a = 10;
+// let b = 10;
+// let c = a+b;
+// console.log(a+b)
+// console.timeEnd()
+
+
+
+// console.log("zeeshan".toUpperCase())
+
+// let river="Zeeshan"
+// let character=river.charAt(5)
+// console.log(character)
+
+// console.log("1000"-"10")
+// a= "ZEEshan"
+// // console.log(a.length)
+// let zee = "java Script Language"
+// console.log(zee.charAt(0))
+// console.log(zee.slice(1))
+// console.log(zee.split(" "))
+// let s = [[asma,sofia,mia],[james,mathew,mars]]
+// console.log(s[0][1])
+// let users=[
+// let user1 = {
+//     name= "mathew",
+//     age= 20
+// }
+// let user2={
+//     name= "Same",
+//     surname = "pinto"
+// }
+// ]
+// console.log(user1.name)
+let nsme=["sam","james","mathew"]
+console.log(nsme.indexOf("james"))
+// nsme[4]="hina"
+// console.log(nsme.length)
+let name = ["jack","rose","samantha","mathew","zeeshan"]
+// name.push("amelia")
+// name.unshift("zeeshan")
+// console.log(name.indexOf(name[0]))
+
+// let popped = name.pop()
+// console.log("Popped=", popped)
+// console.log(name)
+
+// console.log(name.shift())
+// console.log(name)
+
+
+// let rev = name.reverse()
+// console.log("Reversed",rev)
+
+// let n1 = name.slice(2)
+// console.log(n1)
+// let n2 = name.slice(1,3)
+// console.log(n2)
+// let n3= name.slice(1,-1)
+// console.log(n3)
+// let n4= name.slice(-4,-1)
+ // console.log(n4)
+
+//  let all = name.concat(nsme)
+//  console.log("all name", all)
+
+let ticket = {
+    startingStation : "Khairabad",
+    endStation : "Lucknow",
+    price : 350
+}
+console.log(ticket.price)
