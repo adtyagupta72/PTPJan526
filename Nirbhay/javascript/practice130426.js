20b95101b212a0891a803e173d3c4b53774028bb
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-05-02 11:47:31 +0530

    All task and scenerio are Done (complex data type)

--- Nirbhay/javascript/practice130426.js
+++ Nirbhay/javascript/practice130426.js
@@ -15,7 +15,7 @@
 // console.log("Lily",PRICE_SINGLE_LILY,NoOfLily,TotalPriceLily)
 // console.log("Tulip",PRICE_SINGLE_TULIP,NoOfTulip,TotalPriceTulip)
 // console.log("Total Price",Total)
- 
+
 // const PRICE_SINGLE_ROSE=8
 // var NoOfRoses=70-20
 
@@ -35,22 +35,48 @@
 // console.log("Lily",PRICE_SINGLE_LILY,NoOfLily,TotalPriceLily)
 // console.log("Tulip",PRICE_SINGLE_TULIP,NoOfTulip,TotalPriceTulip)
 // console.log("Total Price",Total)
- 
+
 //scenerio2
 
-let FirstName="Maxwell Wright"
-let SecondName="Raja Villarreal"
-let ThirdName="Helen Richards"
+let FirstName = "Maxwell Wright"
+let SecondName = "Raja Villarreal"
+let ThirdName = "Helen Richards"
 
-let Phone1="(0191)7196495"
-let Phone2="08663982895"
-let Phone3="08001111"
+let Phone1 = "(0191)7196495"
+let Phone2 = "08663982895"
+let Phone3 = "08001111"
 
-let email1="maxwell@gmail.com"
-let email2="rajacholavill@gmail.com"
-let email3="helenrichard@gmail.com"
+let email1 = "maxwell@gmail.com"
+let email2 = "rajacholavill@gmail.com"
+let email3 = "helenrichard@gmail.com"
 
-console.log("Name"," ","PhoneNumber"," ","Email")
-console.log(FirstName,"",Phone1,"",email1)
-console.log(SecondName,"",Phone2,"",email2)
-console.log(ThirdName,"",Phone3,"",email3)
+console.log("Name", " ", "PhoneNumber", " ", "Email")
+console.log(FirstName, "", Phone1, "", email1)
+console.log(SecondName, "", Phone2, "", email2)
+console.log(ThirdName, "", Phone3, "", email3)
+
+// changing this to get more good result
+let ContactList = [
+    {
+        name:"Nirbhay",
+        phone:"745474883",
+        email:"nirbhaystp4141@gmail.com"
+    },
+    {
+        name:"Raja Villarreal",
+        phone:"08663982895",
+        email:"rajacholavill@gmail.com"
+    },
+    {
+        name:"Helen Richards",
+        phone:"08001111",
+        email:"helenrichard@gmail.com"
+    }
+]
+// new contact list using complex datatype
+ContactList.push({
+    name:"Maisie Haley",
+    phone:"09135313030",
+    email:"maisehaley2gmail.con"
+})
+console.log(ContactList[0],ContactList[3])
