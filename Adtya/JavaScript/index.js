74165fb34cd3a0a89ac0941fcd4fa2216d7a2a05
Adtya Gupta <adtyagupta72@gmail.com>
2026-07-29 16:48:17 +0530

    JS: Classless objects getter and setter

--- Adtya/JavaScript/index.js
+++ Adtya/JavaScript/index.js
@@ -2370,31 +2370,128 @@ f2(30) => f1(20)(30) => sum(10)(20)(30)
 // console.log("circleCopy === circle1: ", circleCopy === circle1)
 // console.log("circleCopy.center === circle1.center: ", circleCopy.center === circle1.center)
 
-let circle = 
-{
-    radius: 100,
-    center: 
-    {
-        x:0,
-        y:0
-    },
-    // getType: function() 
-    // {
-    //    	return "circle"
- 	// }
-    getType()
-    {
-        // console.log(this.radius)
-        // return "circle"
-        return (typeof this.radius === "number") ? "circle" : "unknown"
-	}
-}
+// let circle = 
+// {
+//     radius: 100,
+//     center: 
+//     {
+//         x:0,
+//         y:0
+//     },
+//     // getType: function() 
+//     // {
+//     //    	return "circle"
+//  	// }
+//     getType()
+//     {
+//         // console.log(this.radius)
+//         // return "circle"
+//         return (typeof this.radius === "number") ? "circle" : "unknown"
+// 	}
+// }
 
 // console.log("Method Calling: ", circle.getType())
 // console.log("Method Calling: ", circle["getType"])
 // console.log("Method Calling: ", circle["getType"]())
-console.log(circle.getType())
-let figure = {...circle}
-delete circle.radius
-console.log(figure.radius)
-console.log(figure.getType())
+// console.log(circle.getType())
+// let figure = {...circle}
+// delete circle.radius
+// console.log(figure.radius)
+// console.log(figure.getType())
+
+// let circle = 
+// {
+//     radius: 100,
+//     x: 100,
+//     y: 200,
+//     center: 
+//     {
+//         x:0,
+//         y:0,
+//         show()
+//         {
+//             console.log(`${this.x}, ${this.y}`)
+//         }
+//     },
+//     show()
+//     {
+//         console.log(`${this.x}, ${this.y}`)
+//     }
+// }
+
+// circle.center.show()
+// circle.show()
+
+// let user = 
+// {
+//     name: "Divya",
+//     city: "Bhopal",
+//     address: 
+//     {
+//     	city: "Indore",
+//     	showCity() 
+//         {
+//      	    console.log("City is:", this.city)
+//    		}
+//  	},
+//     showCity() 
+//     {
+//         console.log("City is:", this.city)
+//     }
+// }
+
+// user.address.showCity()
+// user.showCity()
+
+// let contact = 
+// {
+//   	_tel: "207-662-5412",
+// 	get tel() 
+//     {
+//         console.log("Getter is called!")
+//         return "+91 "+this._tel
+//     },
+//     set tel(t) 
+//     { 
+//         if(t !== "")
+//         {
+//             this._tel = t+"."
+//         }
+//         else
+//         {
+//             console.log("Blank not allowed")
+//         }
+//     }
+// }
+
+// console.log(contact.tel)
+// contact.tel = "100-100-1000"
+// console.log(contact.tel)
+// contact.tel = ""
+// contact.email = "RonaldSMurphy@freepost.org"
+// console.log(contact.email)
+
+let contact = 
+{
+    _age: 36,
+    firstName : "David",
+    lastName : "Taylor",
+    get fullName() 
+    {
+        return `${this.firstName} ${this.lastName}`
+    },
+    get age() 
+    { 
+        return this._age
+    },
+    set age(a) 
+    { 
+        if(a > 0) 
+            this._age = a
+    }
+}
+console.log(contact.fullName)
+contact.age = -20
+console.log(contact.age)
+contact.age = 20
+console.log(contact.age)
