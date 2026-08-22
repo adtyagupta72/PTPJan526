5085c70cb686be8c367687202e0ebbf2366dfb40
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-08-22 21:18:55 +0530

    solving scenerio 4

--- Nirbhay/javascript/classlessObject.js
+++ Nirbhay/javascript/classlessObject.js
@@ -43,3 +43,11 @@ show - which displays all images on the list
 clear – which removes all objects from the list.
 
 While creating an object, use the Image constructor prepared in the earlier task.*/
+
+/*Scenario 4
+
+Complement the images object from the previous task with two new methods (without rewriting the whole object):
+edit - which takes three arguments (title, artist, and date) and if it finds an image with the given title in the list, it changes its artist and date properties
+delete - which takes the title argument and if it finds a picture with this title in the list, it deletes it (to delete a list element, use the splice method)
+
+Additionally, add a show method to the Image constructor, which will display information about this one image. Do not rewrite the constructor. Use prototypes for this purpose. Then modify the show method of the images object so that it uses the newly created single image show method to display the information.*/
