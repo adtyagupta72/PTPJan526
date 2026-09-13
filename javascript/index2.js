6aeead23b64ecdb03c1d077bca1d79f70ebd8ed7
zeeshan <zeeshan6767siddiqui@gmail.com>
2026-09-13 23:49:23 +0530

    array basic

--- javascript/index2.js
+++ javascript/index2.js
@@ -2338,3 +2338,10 @@ console.log("zeeshan2")
 
 console.log("zeeshan2")
 console.log("zeeshan2")
+
+
+let a = [10, 20, "en to tre", true, 50];
+a[4] = a[4] * 2;
+console.log(a[0]);
+console.log(a[2]);
+console.log(a[4]);
