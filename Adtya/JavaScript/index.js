cf0b8ba4260cd60404b703a6e1d9957b13c72765
Adtya Gupta <adtyagupta72@gmail.com>
2026-09-15 19:57:51 +0530

    JS: Built-in methods - Date completed

--- Adtya/JavaScript/index.js
+++ Adtya/JavaScript/index.js
@@ -3561,10 +3561,15 @@ diff => > (7 days~) mill => 5 marks => solved before 7 days from now
 // let endTime = Date.now()
 // console.log(endTime - startTime) 
 
-let date = new Date("2026-09-14T20:28:00")
-console.log(date.getMonth()) // -> 6 
-console.log(date.getDay()) // -> 3
-console.log(date.getDate()) // 8
-console.log(date.getHours()) // -> 10
-date.setHours(12)
-console.log(date.getHours()) // -> 12
+// let date = new Date("2026-09-14T20:28:00")
+// console.log(date.getMonth()) // -> 6 
+// console.log(date.getDay()) // -> 3
+// console.log(date.getDate()) // 8
+// console.log(date.getHours()) // -> 10
+// date.setHours(12)
+// console.log(date.getHours()) // -> 12
+
+let date = new Date("2020-07-08T10:20:00")
+console.log(date.toLocaleDateString()) // -> 08/07/2020
+console.log(date.toLocaleTimeString()) // -> 10:20:00
+
